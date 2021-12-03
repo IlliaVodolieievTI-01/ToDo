6aaeb1a0096482f693b0d2955b9431c31cfb334b
Illia Vodolieiev TI-01 <illuhavoda2003@gmail.com>
2021-12-03 21:58:31 +0200

    models

--- to_do/to_do_app/admin.py
+++ to_do/to_do_app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+class ToDoListAdmin(admin.ModelAdmin):
+    list_display = ('task', 'start_data', 'end_data', 'priority')
+    search_fields = ('task', 'start_data')
+    list_filter = ('start_data', 'end_data', 'priority')
+
+admin.site.register(ToDoList, ToDoListAdmin)
--- to_do/to_do_app/models.py
+++ to_do/to_do_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class ToDoList(models.Model):
+    CHOICES = (
+        ('1', 'priority1'),
+        ('2', 'priority2'),
+        ('3', 'priority3'),
+        ('4', 'priority4')
+    )
+    task = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    start_data = models.DateField()
+    end_data = models.DateField(blank=True)
+    time = models.TimeField()
+    priority = models.CharField(max_length=10, choices=CHOICES)
